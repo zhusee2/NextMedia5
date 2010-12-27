251dc392db37cb64c4669b6576bad76b1535790d
Zhusee Zhang <zhusee2@gmail.com>
2010-12-27 11:57:16 +0800

    Version 1.1.0
    Add play button before video load
    Fix for layout change of Apple Daily website

--- initialize.js
+++ initialize.js
@@ -1,7 +1,7 @@
 /*
 
 NextMedia5 Safari Extension
-Version 1.0.0
+Version 1.1.0
 By Zhusee (zhusee@gmail.com)
 
 Visit: http://zhusee2.github.com/nextmedia5
@@ -17,8 +17,21 @@ document.addEventListener('beforeload', function(event) {
     
     var videoSrc = flashVideoSrc.replace(/\/video\//,'/wap_video/') + newsID + '.m4v';
 
-    $(event.target.parentElement).append('<video id="nextmedia5Player" width="660" height="420" controls preload="none" poster="' + posterSrc + '"><source src="' + videoSrc +'" /></video>');
+    $(event.target.parentElement)
+      .addClass('nextmedia5Container')
+      .append('<video id="nextmedia5Player" width="630" height="355" preload="none" poster="' + posterSrc + '"><source src="' + videoSrc +'" /></video>')
+      .append('<div id="nextmedia5PlayButton"><a href="#">▲</a></div>');
     $(event.target).remove();
+
+    $('#nextmedia5PlayButton a').live('click', function(e) {
+      var player = $('#nextmedia5Player').dom[0];
+      
+      player.controls = true;
+      player.play();
+      
+      $(this.parentElement).remove();
+      e.preventDefault();
+    });
 
     event.preventDefault();
 
