4d4fcc7af57592a27f5376920fb89e70d56f6293
Zhusee Zhang <zhusee2@gmail.com>
2011-02-01 22:59:33 +0800

    imporve video url handing with fallback resources

--- NextMedia5.safariextension/initialize.js
+++ NextMedia5.safariextension/initialize.js
@@ -16,13 +16,18 @@ document.addEventListener('beforeload', function(event) {
     var videoHash = posterSrc.match(/\d\/(.*)_\d*.jpg/)[1];
     
     var videoSrcCandidate = flashVideoSrc.replace(/\/video\/\//,'/wap_video/')
-          .replace(/\.flv$/,'.m4v');
+                              .replace(/\.flv$/,'.m4v');
     var videoSrc = videoSrcCandidate.replace(/\w*.m4v/,videoHash + '.m4v');
 
     $(event.target.parentElement)
       .addClass('nextmedia5Container')
-      .append('<video id="nextmedia5Player" width="630" height="355" preload="none" poster="' + posterSrc + '"><source src="' + videoSrc +'" type="video/mp4" /><source src="' + videoSrcCandidate +'" type="video/mp4" /><source src="' + flashVideoSrc +'" type="video/x-flv" /></video>')
+      .append('<video id="nextmedia5Player" width="630" height="355" preload="none" poster="' + posterSrc + '"><source src="' + videoSrc +'" type="video/mp4" /><source src="' + flashVideoSrc +'" type="video/x-flv" /></video>')
       .append('<div id="nextmedia5PlayButton"><a href="#">▲</a></div>');
+    
+    if (videoSrcCandidate !== videoSrc) {
+      $('video#nextmedia5Player source:first-child').after('<source src="' + videoSrcCandidate +'" type="video/mp4" />');
+    }
+    
     $(event.target).remove();
 
     $('#nextmedia5PlayButton a').live('click', function(e) {
